03ddb05a03163aeb79385a03bc49f0ff5feae73b
lavenderdotpet <51702517+lavenderdotpet@users.noreply.github.com>
2025-12-15 17:54:53 +0200

    BUILD: pushed compile masp and wads scripts
    pushed compile map and wad scripts

---
+++ texture-wads/compilewads.sh
@@ -0,0 +1,8 @@
+# licence cc0/bsd0/mit0 (not that i really think licences make sense for bash scripts)
+qpakman qcompat_base/*.png -o base.wad | grep failures
+qpakman qcompat_jrmed/*.png -o jr_med.wad | grep failures
+qpakman qcompat_med/*.png -o medieval.wad | grep failures
+qpakman qcompat_metal/*.png -o metal.wad | grep failures
+qpakman qcompat_start/*.png -o start.wad | grep failures
+qpakman qcompat_tim/*.png -o tim.wad | grep failures
+qpakman qcompat_wizard/*.png -o wizard.wad | grep failures
